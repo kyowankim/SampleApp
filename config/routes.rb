a5d9299ad6272f1e99353acc4da137f3875db3e3
kyowankim <kyowangkim@gmail.com>
2022-01-09 17:18:16 -0500

    Chapter 8

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'session/new'
   get 'users/new'
   root 'static_pages#home' 
   get 'static_pages/home'
@@ -6,5 +7,9 @@ Rails.application.routes.draw do
   get '/about', to: 'static_pages#about', as: :about
   get '/contact' => 'static_pages#contact', as: :contact  # => and to: are essentially the same syntax
   get '/signup' => 'users#new'
+  get '/login' => "session#new" 
+  post '/login' => "session#create"
+  delete '/logout' => "session#delete"
+  resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
