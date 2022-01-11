2240e3b6cddcd04d5542e973dd95918ffd1248b1
kyowankim <kyowangkim@gmail.com>
2022-01-10 23:00:44 -0500

    Implement advanced login

--- app/controllers/session_controller.rb
+++ app/controllers/session_controller.rb
@@ -7,6 +7,7 @@ class SessionController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       reset_session
+      params[:session][:remember_me] == '1' ? remember(user) : forget(user)
       log_in(user)
       redirect_to user
     else
@@ -16,7 +17,7 @@ class SessionController < ApplicationController
   end
 
   def delete
-    log_out
+    log_out if logged_in?
     redirect_to root_path
   end
 
